30259f7ac5d3664b28164ea675ae8e780cf0aed7
coil398 <konayuta.sou@gmail.com>
2017-09-25 19:02:10 +0900

    docker automation

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,11 +1,10 @@
-docker pull ubuntu:16.04
-
 if [ -e ./pepper ]; then
     echo 'folder pepper exists'
 else
     mkdir pepper
 fi
 
+wget https://goo.gl/zyM4fk
 docker build -t pepper:python .
 
 docker run -v $HOME/docker/pepper -it --name pepper pepper:python /bin/bash
