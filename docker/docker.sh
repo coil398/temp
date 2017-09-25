a08f368df91bfd7004a26ba1a34721d0fca6616c
coil398 <konayuta.sou@gmail.com>
2017-09-25 19:05:51 +0900

    added -O option

--- docker/docker.sh
+++ docker/docker.sh
@@ -4,7 +4,7 @@ else
     mkdir pepper
 fi
 
-wget https://goo.gl/zyM4fk
+wget -O Dockerfile https://goo.gl/zyM4fk
 docker build -t pepper:python .
 
 docker run -v $HOME/docker/pepper -it --name pepper pepper:python /bin/bash
