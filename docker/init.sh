f77a1187e6f26a935bf61d314ce3c9867617540c
coil398 <konayuta.sou@gmail.com>
2017-09-25 18:34:12 +0900

    added curl

--- docker/init.sh
+++ docker/init.sh
@@ -1,3 +1,4 @@
 brew tap caskroom/cask
 brew cask install docker
-
+curl -fsSL https://raw.githubusercontent.com/coil398/temp/master/docker/docker.sh
+curl -fsSL https://raw.githubusercontent.com/coil398/temp/master/docker/Dockerfile
