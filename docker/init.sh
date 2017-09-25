f9c38c8dd1dfde372c979afcbda9a03ff823b1f5
coil398 <konayuta.sou@gmail.com>
2017-09-25 18:42:42 +0900

    deleted init.sh

--- docker/init.sh
+++
@@ -1,4 +0,0 @@
-brew tap caskroom/cask
-brew cask install docker
-curl -fsSL https://raw.githubusercontent.com/coil398/temp/master/docker/docker.sh
-curl -fsSL https://raw.githubusercontent.com/coil398/temp/master/docker/Dockerfile
